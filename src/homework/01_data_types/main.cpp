51a73f48490ee69f9770b0449ae4d92831f4c96a
esleiderZ <esleider.duboy@g.austincc.edu>
2025-09-11 04:29:11 +0000

    echo_variable w unit test case

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,20 @@
 ﻿//write include statements
+#include <iostream>
 
+using std::cout;
 
-//write namespace using statement for cout
-
+int multiply_numbers(int num1, int num2)
+{
+    return num1 * num2;
+}
 
 int main()
 {
-	
+    int result1 = multiply_numbers(7, 7);
+    cout << "7 x 7 = " << result1 << "\n";
 
-	return 0;
+    int result2 = multiply_numbers(5, 5);
+    cout << "5 x 5 = " << result2 << "\n";
+
+    return 0;
 }
