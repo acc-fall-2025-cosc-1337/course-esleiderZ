d62fa52b3bc5c5c554c17401870ce52f9109d378
esleiderZ <esleider.duboy@g.austincc.edu>
2025-09-15 21:38:26 +0000

    Homework_01

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -8,6 +8,30 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
+int multiply_numbers(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+// #include <iostream>
+
+// using std::cout;
+
+// int multiply_numbers(int num1, int num2)
+// {
+//     return num1 * num2;
+// }
+
+// int main()
+// {
+//     int result1 = multiply_numbers(7, 7);
+//     cout << "7 x 7 = " << result1 << "\n";
+
+//     int result2 = multiply_numbers(5, 5);
+//     cout << "5 x 5 = " << result2 << "\n";
+
+//     return 0;
+// } This was my another try that i did with my knowledge so far on c++ but im not sure if its correct or not.
 
 
 
--- src/homework/01_data_types/data_types.h
+++ src/homework/01_data_types/data_types.h
@@ -2,7 +2,7 @@
 int add_numbers(int num1, int num2);
 
 //write function prototype here
+// int echo_variable(int num1);//proptype/header/signature 1st try.
 
-
-
+int multiply_numbers(int num1, int num2);//proptype/header/signature
 
