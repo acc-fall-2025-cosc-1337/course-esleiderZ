ec78287452fe001ee6c2dc98934f5818eb18361d
esleiderZ <esleider.duboy@g.austincc.edu>
2025-09-20 04:54:43 +0000

    HW_02

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -8,6 +8,16 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
+#include <iostream>
 
+const double tax_rate = 0.0675;
+
+double get_sales_tax_amount(double meal_amount) {
+    return meal_amount * tax_rate;
+}
+
+double get_tip_amount(double meal_amount, double tip_rate) {
+    return meal_amount * (tip_rate / 100.0);
+}
 
 
