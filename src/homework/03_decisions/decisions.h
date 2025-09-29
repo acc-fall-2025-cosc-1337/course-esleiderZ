4d1e5ad8f12b5d5c8277f20ac9629758712f2188
Leonardo Bastidas <38115827+cybergoatpsyops@users.noreply.github.com>
2025-09-29 23:15:42 +0000

    homework3 late, grade using if and switch statements

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,7 +1,10 @@
 //write include required statements
+#include <string>
 
 
 //Write a function(s) prototype  here
+std::string get_letter_grade_using_if(int numerical_grade);
+std::string get_letter_grade_using_switch(int numerical_grade);
 
 
 
